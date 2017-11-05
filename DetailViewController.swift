66f532de73fab88f5dff8b3d351c7b9b2c7f2797
Skyler Svendsen <skylersvendsen@gmail.com>
2017-11-05 16:11:56 -0600

    updated to iOS 11.0, added save photos ability

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = selectedImage
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped(){
+        
+//        if let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+//            vc.setInitialText("Look at this great picture!")
+//            vc.add(imageView.image!)
+//            vc.add(URL(string: "http://www.photolib.noaa.gov/nss1"))
+//            present(vc, animated: true)
+//        }
+        
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
